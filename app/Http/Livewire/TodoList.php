1faeb404bebe9cfd2ade1748b7d73b9f68e64990
shadowgks <saad.moumou.coder@gmail.com>
2023-05-05 21:46:07 +0100

    project Todolist done

---
+++ app/Http/Livewire/TodoList.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Models\Todolists;
+use Livewire\Component;
+
+class TodoList extends Component
+{
+    public string $todo_lists = '';
+    public $todos;
+
+    public function mount()
+    {
+        $this->todoLists();
+    }
+
+    public function todoLists()
+    {
+        $this->todos = Todolists::orderBy('created_at', 'Desc')->get();
+    }
+
+    public function addTodoList()
+    {
+        $create = Todolists::create([
+            'title' => $this->todo_lists,
+            'complated' => false,
+        ]);
+        $this->todo_lists = '';
+        $this->todoLists();
+    }
+
+    public function toggleTodoList($id)
+    {
+        $toggle = Todolists::where('id', $id)->first();
+        if (!$toggle) {
+            return;
+        }
+        $toggle->complated = !$toggle->complated;
+        $toggle->save();
+        $this->todoLists();
+    }
+
+    public function distoryTodoList($id)
+    {
+        $find_item = Todolists::find($id);
+        if (!$find_item) {
+            return;
+        }
+        $find_item->delete();
+        $this->todoLists();
+    }
+
+    public function render()
+    {
+        return view('livewire.todo-list');
+    }
+}
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,5 @@
 <x-app-layout>
+    <livewire:todo-list />
     <livewire:calculator />
     <livewire:counter />
 </x-app-layout>
