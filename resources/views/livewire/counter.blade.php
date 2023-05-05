aec3ffe92e260487acb2dd3cb70cbc16d18a2ffb
shadowgks <saad.moumou.coder@gmail.com>
2023-05-05 18:28:29 +0100

    now begin project calculator

--- resources/views/livewire/counter.blade.php
+++ resources/views/livewire/counter.blade.php
@@ -1,5 +1,5 @@
-<div>
-    <button wire:click="Increment">+</button>
+<div class="flex justify-center gap-7 items-center p-16">
+    <button class="bg-blue-600 text-white px-2 rounded-full" wire:click="Increment">+</button>
     <span>{{$count}}</span>
-    <button wire:click="Decrement">-</button>
+    <button class="bg-blue-600 text-white px-2 rounded-full" wire:click="Decrement">-</button>
 </div>
