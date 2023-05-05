b6af2c5e2a22454df49cb064a4bcf5ae92f92516
shadowgks <saad.moumou.coder@gmail.com>
2023-05-05 19:43:41 +0100

    install tailwind forms

--- resources/views/livewire/calculator.blade.php
+++ resources/views/livewire/calculator.blade.php
@@ -1,15 +1,15 @@
 <div class="flex gap-6 justify-center items-center p-16">
-    <input class="border border-black pl-3" type="text" wire:model="number1">
-    <select class="border border-black" wire:model="actions" name="" id="">
+    <input type="text" wire:model="number1">
+    <select wire:model="actions" name="" id="">
         <option>+</option>
         <option>-</option>
         <option>*</option>
         <option>/</option>
         <option>%</option>
     </select>
-    <input class="border border-black pl-3" type="text" wire:model="number2">
+    <input type="text" wire:model="number2">
     <button wire:click="calculator"
-    class="bg-blue-600 text-white px-3 rounded-xl disabled:cursor-not-allowed disabled:bg-opacity-90"
+    class="bg-blue-600 text-white py-2 px-3 rounded-sm disabled:cursor-not-allowed disabled:bg-opacity-90"
     {{$disabled ? 'disabled' : ''}}>=</button>
 
     {{-- Resulte Final--}}
