456b39518da2f9c687b025870900afec5501a411
shadowgks <saad.moumou.coder@gmail.com>
2023-05-05 17:41:50 +0100

    create env for working && create components && install tailwind && 1 project Counter Done

---
+++ app/Http/Livewire/Counter.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class Counter extends Component
+{
+    public $count = 0;
+    protected $debug = true;
+
+    public function Increment()
+    {
+        $this->count++;
+    }
+
+    public function Decrement()
+    {
+        $this->count--;
+    }
+
+    public function render()
+    {
+        return view('livewire.counter');
+    }
+}
---
+++ resources/views/livewire/counter.blade.php
@@ -0,0 +1,5 @@
+<div>
+    <button wire:click="Increment">+</button>
+    <span>{{$count}}</span>
+    <button wire:click="Decrement">-</button>
+</div>
